a312b5a8a68d637f438e731b9ef1259ba87646b0
Isaac Silvério <isaacjs.teste@gmail.com>
2019-04-06 16:26:35 -0300

    iniciando sistema de controle

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+   <head>
+      <title> Controlfip</title>
+   </head>
+   <body>
+	<?php echo "Olá Mundo! Está tudo ok posso começar a desenvolver"; ?>
+   </body>
+</html>
